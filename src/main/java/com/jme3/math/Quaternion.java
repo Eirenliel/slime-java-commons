55aba1bf93126294122e427e785deaecc6928c27
Eiren Rain <eirenliel@users.noreply.github.com>
2021-02-04 03:04:22 +0300

    Added AWTThread annotation Fixed comments in euler angles in quaternion for opengl axises

--- src/main/java/com/jme3/math/Quaternion.java
+++ src/main/java/com/jme3/math/Quaternion.java
@@ -317,9 +317,9 @@ public final class Quaternion implements Cloneable, java.io.Serializable {
 			angles[2] = -FastMath.HALF_PI;
 			angles[0] = 0;
 		} else {
-			angles[1] = FastMath.atan2(2 * y * w - 2 * x * z, sqx - sqy - sqz + sqw); // roll or heading 
-			angles[2] = FastMath.asin(2 * test / unit); // pitch or attitude
-			angles[0] = FastMath.atan2(2 * x * w - 2 * y * z, -sqx + sqy - sqz + sqw); // yaw or bank
+			angles[1] = FastMath.atan2(2 * y * w - 2 * x * z, sqx - sqy - sqz + sqw); // yaw or bank
+			angles[2] = FastMath.asin(2 * test / unit); // roll or heading
+			angles[0] = FastMath.atan2(2 * x * w - 2 * y * z, -sqx + sqy - sqz + sqw); // pitch or attitude
 		}
 		return angles;
 	}
